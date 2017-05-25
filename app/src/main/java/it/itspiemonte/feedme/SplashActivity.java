421b2016f8602435aedf6112e10599b480ba3ed5
toqir nasir <toqir.nasir@edu.itspiemonte>
2017-05-25 12:30:38 +0200

    prova da toq a marius 2

--- app/src/main/java/it/itspiemonte/feedme/SplashActivity.java
+++ app/src/main/java/it/itspiemonte/feedme/SplashActivity.java
@@ -2,6 +2,7 @@ package it.itspiemonte.feedme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Log.i("Prova FeedMe","prova mook");
     }
 }
